9fbd9d084aaa738b627b66ead5e001c16a0c9597
Julia <julia.sperandio@minu.co>
2022-06-28 00:38:25 -0300

    fix: changing styles of description page

--- src/pages/description/index.jsx
+++ src/pages/description/index.jsx
@@ -1,15 +1,45 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
 
+import { ChoosedMovieContext } from '../../contexts/choosedMovie';
 import BackButton from '../../components/BackButton';
+import TagGender from '../../components/TagGenre';
+import {
+	HighlightDescription,
+	Wrapper,
+	DescriptionContainer,
+	Image,
+	DescriptionText
+} from './style';
 
 function Description() {
-	let { id } = useParams();
+	const { choosedMovie } = useContext(ChoosedMovieContext);
+
+	console.log(choosedMovie);
 
 	return (
-		<div>
-			<BackButton />
-			<p>description {id}</p>
-		</div>
+		<Wrapper>
+			<HighlightDescription>
+				<BackButton />
+			</HighlightDescription>
+			<DescriptionContainer>
+				<Image
+					src={`https://image.tmdb.org/t/p/w500${choosedMovie.poster_path}`}
+					alt='ola'
+				/>
+				<DescriptionText>
+					<TagGender value={choosedMovie.genre_ids} />
+					<p>Título: </p>
+					<span>{choosedMovie.title}</span>
+					<p>Lançamento: </p>
+					<span>{choosedMovie.release_date}</span>
+					<p>Resumo: </p>
+					<span>{choosedMovie.overview}</span>
+					<p>Nota:</p>
+					<span> {choosedMovie.vote_average}</span>
+				</DescriptionText>
+			</DescriptionContainer>
+		</Wrapper>
 	);
 }
 
---
+++ src/pages/description/style.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+// import { devices } from '../../utils/breakpoints';
+
+export const HighlightDescription = styled.div`
+	max-width: 1280px;
+	height: fit-content;
+	margin: 50px auto 20px;
+	padding-left: 30px;
+	border-radius: 8px;
+	background-color: #2d0c5e;
+	box-shadow: 0px 8px 20px -3px rgba(0, 0, 0, 0.7);
+`;
+
+export const Wrapper = styled.div`
+	max-width: 1280px;
+	height: 100%;
+	margin: 50px auto 0;
+`;
+
+export const DescriptionContainer = styled.div`
+	width: 100%;
+	max-width: 1280px;
+	height: fit-content;
+	margin: 0 auto;
+
+	display: flex;
+`;
+
+export const Image = styled.img`
+	max-width: 350px;
+	max-height: 450px;
+	border-radius: 16px;
+	box-shadow: 0px 8px 20px -3px rgba(0, 0, 0, 0.7);
+`;
+
+export const DescriptionText = styled.div`
+	max-width: 800px;
+	height: fit-content;
+	margin-left: 20px;
+	padding: 20px 30px;
+	border-radius: 16px;
+	border: 2px solid gray;
+	background-color: #4b5358;
+	font: 16px Roboto, sans-serif;
+	color: #ffffff;
+	line-height: 23px;
+	box-shadow: 0px 8px 20px -3px rgba(0, 0, 0, 0.7);
+
+	p {
+		margin-bottom: 8px;
+		font-size: 24px;
+		font-weight: 900;
+	}
+
+	span {
+		font-size: 18px;
+	}
+`;
