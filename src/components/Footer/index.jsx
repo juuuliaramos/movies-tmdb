640bdd4559cbbf271fe7c1258f3b93e89984e143
Julia <julia.sperandio@minu.co>
2022-06-28 00:34:00 -0300

    feat: added Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import { FooterWrapper, Content, Icon } from './style';
+
+function Footer() {
+	return (
+		<FooterWrapper data-testid='headerWrapper'>
+			<Content href='https://www.linkedin.com/in/juliasperandio/'>
+				<Icon />
+			</Content>
+		</FooterWrapper>
+	);
+}
+export default Footer;
---
+++ src/components/Footer/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { TbBrandLinkedin } from 'react-icons/tb';
+
+import { devices } from '../../utils/breakpoints';
+
+export const FooterWrapper = styled.div`
+	width: 100%;
+	height: fit-content;
+	background-color: #5c16c5;
+	margin: 0 auto;
+
+	position: fixed;
+	bottom: 0;
+`;
+
+export const Content = styled.a`
+	width: 100%;
+	max-width: 1260px;
+	height: 50px;
+	margin: 0 auto;
+
+	display: flex;
+	justify-content: center;
+
+	/* a:link,
+	a:active,
+	a:visited {
+		text-decoration: none;
+		font-size: 16px;
+		font-family: Roboto, sans-serif;
+	} */
+`;
+
+export const Icon = styled(TbBrandLinkedin)`
+	width: 34px;
+	height: 50px;
+`;
